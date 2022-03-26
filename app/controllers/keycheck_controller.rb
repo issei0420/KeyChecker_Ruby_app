b68d4d33f1bfcbd8f24ac4095f2069401a57ec90
齋木一晴 <120420issei@gmail.com>
2022-03-26 16:07:05 +0900

    main関数実装完了

--- app/controllers/keycheck_controller.rb
+++ app/controllers/keycheck_controller.rb
@@ -1,4 +1,11 @@
 class KeycheckController < ApplicationController
+    //キーを判定する関数
+    def keycheck (keys)
+        key = ''
+        return key
+    end
+    
     def top
+        @key = keycheck()
     end
 end
