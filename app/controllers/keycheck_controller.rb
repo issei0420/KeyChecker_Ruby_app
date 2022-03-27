7c795987f1331bb37f505ed702a1b396cf419dcf
齋木一晴 <120420issei@gmail.com>
2022-03-27 15:09:48 +0900

    keychecktop作成

--- app/controllers/keycheck_controller.rb
+++ app/controllers/keycheck_controller.rb
@@ -1,11 +1,8 @@
+require "./app/keycheck_function.rb"
 class KeycheckController < ApplicationController
-    //キーを判定する関数
-    def keycheck (keys)
-        key = ''
-        return key
-    end
-    
+
     def top
-        @key = keycheck()
     end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get '/' => "home#top"
-  get '/keycheck' => "keycheck#top"
+  get '/keycheck' => "keycheck#keychecktop"
   get '/songlist' => "songlist#index"
 end
