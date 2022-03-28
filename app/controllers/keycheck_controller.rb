03663e144749301dcf05e9d0a33f389c71a8c760
齋木一晴 <120420issei@gmail.com>
2022-03-28 10:58:53 +0900

    params配列で渡す処理実装

--- app/controllers/keycheck_controller.rb
+++ app/controllers/keycheck_controller.rb
@@ -1,9 +1,17 @@
+require "./app/keycheck_function"
 class KeycheckController < ApplicationController
 
     def top
     end
     
     def check_key
-        @input_codes = params[:codes]
+        @params = params
+        # input_codes = []
+        # 6.times do |timesCount|
+        #     input_node = params["node#{timesCount}"]             
+        #     input_acc = params["acc#{timesCount}"]
+        #     code = input_node + input_acc
+        #     @input_codes.add(code)
+        # end
     end
 end
